0500f983398c768561fff8e3d8e0fcb2be38e693
jufelvii <jufelvii29@gmail.com>
2019-07-26 14:35:47 +0800

    upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+function light(sw) {
+  var pic;
+  if (sw == 0) {
+    pic = "pic_bulboff.gif"
+  } else {
+    pic = "pic_bulbon.gif"
+  }https://www.w3schools.com/
+  document.getElementById('myImage').src = pic;
+}
+</script>
