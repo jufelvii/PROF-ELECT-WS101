ea6d4f10c420e7001a4df91c8d8e5f0fa3c09326
jufelvii <jufelvii29@gmail.com>
2019-07-26 14:38:04 +0800

    upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-<script>
+
 function light(sw) {
   var pic;
   if (sw == 0) {
-    pic = "pic_bulboff.gif"
+    pic = "img/off.gif"
   } else {
-    pic = "pic_bulbon.gif"
+    pic = "img/on.gif"
   }https://www.w3schools.com/
   document.getElementById('myImage').src = pic;
 }
-</script>
